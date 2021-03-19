a201547d38e15b8e60f95d599e811500ac166854
Michiel Wouters <michiel_woute64@hotmail.com>
2021-03-19 13:55:12 +0100

    Add project files.

---
+++ Instagramme/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Instagramme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userRepo = new UserRepository();
+            userRepo.GetXAmountOfUsers(10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Instagramme/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instagramme
+{
+    public class UserRepository
+    {
+        // A connectionstring tells an app which DB it can use to store / fetch data
+        private const string connectionString = @"
+                Data Source=.\SQLEXPRESS;Initial Catalog=Instagramme;Integrated Security=True";
+
+        public void GetAllUsers()
+        {
+            string query = "SELECT * FROM [Instagramme].[dbo].[Users]";
+
+            GetData(query);
+        }
+
+        public void GetXAmountOfUsers(int amountOfUsers)
+        {
+            string query = $"SELECT * FROM [Instagramme].[dbo].[Users] WHERE UserId <= {amountOfUsers}";
+            GetData(query);
+        }
+
+        private void GetData(string query)
+        {
+            // A connection has to be open between app and DB to allow for communication
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // A command will execute a query on an open connection
+                SqlCommand command = new SqlCommand(query, connection);
+
+                // We have to manually open a connection
+                // Open late, close early -> An open connection is precious.
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                ReadAllRows(reader);
+            }
+        }
+
+        private void ReadAllRows(SqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+                string row = $"{reader.GetInt32(0)}, {reader.GetString(1)}, {reader.GetDateTime(2)}";
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
